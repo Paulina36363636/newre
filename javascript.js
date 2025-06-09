08063402630ab57ed2a57028315f1167f8f73809
unknown <opondopaulina@gmail.com>
2025-06-09 10:11:57 +0300

    seeing my changes of what i have done

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        alert("Button clicked!");
+    });
+});
+$("button").click(function(){
+  $("p").hide();
+});
+$("#ourlist").mouseleave(function(){
+  alert("you understand the list best");
+});
+
